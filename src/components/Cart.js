43c0e14e55971f034e7dabf21081fdeba0ca54e7
Jelena Gruica <jelena.gruica@gmail.com>
2018-02-12 22:06:29 -0800

    working on style

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -19,36 +19,54 @@ class Cart extends Component {
                 const picturePath = '/images/' + item.filename
                 return (
                     <li key={index}>
-                        <img src={picturePath} alt="" />
-                        {item.name} <br />
-                        {this.formatPrice(item.price)}
-                        <button onClick={() => this.props.removeFromCart(index)}> x </button>
+                    <div className='removeDiv'>
+                            <button className='remove' onClick={() => this.props.removeFromCart(index)}> x </button>
+                        </div>
+                        <div className='items'>
+                            <p className='text'>{item.name} </p>
+                            <p className='price'>{this.formatPrice(item.price)}</p>
+                        </div>
+                        <div className='imgDiv'>
+                            <img className='cartImg' src={picturePath} alt='pic' />
+                        </div>
                     </li>
                 )
             })
             return (
-                <div className="cart">
-                    Your Cart
-                <div className="cart-list">
-                        <ul>
-                            {list}
-                        </ul>
-                        <hr />
-                        Total {this.formatPrice(this.calculateTotalPrice())}
-                        <br />
-                        <button onClick={this.props.clickBack}> BACK </button>
+                <div className='container-cart'>
+                    <div className='cart'>
+                        <h3 className='center'> Your Cart </h3>
+                        <div className='cart-list'>
+                            <ul>
+                                {list}
+                            </ul>
+                            <hr />
+                            <div>
+                                <div className='totalPrice'>{this.formatPrice(this.calculateTotalPrice())}</div>
+                                <div className='total'> Total </div>
+                            </div>
+                            <div className='center'>
+                                <button className='back' onClick={this.props.clickBack}> Back </button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             )
         } else {
             return (
-                <div className="empty-cart">
-                    <h2> Your Cart </h2>
-                    Nothing in your cart, <br/> start shopping.
-                    <hr />
-                    Total {this.formatPrice(this.calculateTotalPrice())}
-                    <br />
-                    <button onClick={this.props.clickBack}> BACK </button>
+                <div className='container-empty-cart'>
+                    <div className='cart-empty'>
+                        <h3 className='center'> Your Cart </h3>
+                        <p className='center'>Nothing in your cart, <br /> start shopping.</p>
+                        <hr />
+                        <div>
+                            <div className='totalPrice'>{this.formatPrice(this.calculateTotalPrice())}</div>
+                            <div className='total'> Total </div>
+                        </div>
+                        <div className='center'>
+                            <button className='back' onClick={this.props.clickBack}> Back </button>
+                        </div>
+                    </div>
                 </div>
             )
 
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -4,24 +4,26 @@ class Shop extends Component {
 
     calculatePrice = (price) => {
         return price / 100;
-    } 
+    }
 
     render() {
         const list = this.props.products.map((item, index) => {
             const picturePath = '/images/' + item.filename
             return (
                 <li key={index}>
-                    <img src={picturePath} alt=""/> <br />
+                    <img className='img' src={picturePath} alt='pic' /> <br />
                     {item.name} <br />
-                    ${this.calculatePrice(item.price)} <br/>
+                    ${this.calculatePrice(item.price)} <br />
                     <button onClick={() => this.props.addToCart(item)}> Add to cart</button>
                 </li>
             )
         })
         return (
-            <div className="shop">
-                Shop our featured collection
-            <div className="list">
+            <div className='shop'>
+                <div className='shopText'>
+                    Shop our featured collection
+                </div>
+                <div className='list'>
                     <ul>
                         {list}
                     </ul>
